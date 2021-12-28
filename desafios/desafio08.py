197fcfa9b2387798f308ce27a6466fc6c5f7148a
Andressa Lemes <89918957+acllemes@users.noreply.github.com>
2021-12-28 17:09:38 -0300

    Update desafio08.py

--- desafios/desafio08.py
+++ desafios/desafio08.py
@@ -1,2 +1,16 @@
 # escreva um programa que leia um valor em metros e o exiba convertido em centimetro e milimetros
 
+medida = float(input('Uma distância em metros: '))
+mm = medida * 1000 # milimetro
+cm = medida * 100 # centimento
+dm = medida * 10 # decimetro
+dam = medida / 10 # decâmetro
+hm = medida / 100 # hectômetro
+km = medida / 1000 # quilometro
+print('Você digitou {}, segue abaixo mais detalhes sobre a medida declarada'.format(medida))
+print('Em milimetros é {}'.format(mm))
+print('Em centimentos é {}'.format(cm))
+print('Em decimetro é {}'.format(dm))
+print('Em decâmetro é {}'.format(dam))
+print('Em hectômetro é {}'.format(hm))
+print('Em quilômetro é {}'.format(km))
