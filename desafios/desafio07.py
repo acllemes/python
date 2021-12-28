87344ce2762ca0085f9fdd857f7d96b496171054
Andressa Lemes <89918957+acllemes@users.noreply.github.com>
2021-12-28 17:08:39 -0300

    Update desafio07.py

--- desafios/desafio07.py
+++ desafios/desafio07.py
@@ -1,3 +1,8 @@
 # desenvola um programa que leia as duas notas de um aluno
 # calcule e mostre a sua média
 
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+media = (n1 + n2) / 2
+print('A média entre {} e {} é igual a {}'.format(n1, n2, media))
+print('A soma das notas é {} e a media é {}'.format((n1+n2), media))
